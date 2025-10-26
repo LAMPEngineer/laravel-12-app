2a8c2153a41b3b07b2ca7404853906ec6b97c8cc
LAMPEngineer <lamp.engineer@gmail.com>
2025-10-26 10:20:14 +0530

    Eloquent ORM

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,6 +3,8 @@
     <x-slot:title>Jobs Page</x-slot:title>
     <x-slot:heading>Jobs</x-slot:heading>
 
+    <a href="/jobs" class="text-blue-500 hover:underline">Go Back...</a>
+
     <h1> Job</h1>
 
     <h2 class="font-black text-lg">{{ $job['title'] }}</h2>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\Job;
 
 
 Route::get('/', function () {
+
     return view('home');
 });
 
