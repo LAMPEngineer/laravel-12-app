dce27ef860f377910c632e98184c5fa7845c6d3d
LAMPEngineer <lamp.engineer@gmail.com>
2025-11-16 10:28:46 +0530

    resource route

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,8 @@ use App\Models\Job;
 
 
 Route::view('/', 'home' );
-
-Route::controller(JobController::class)->group(function() {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-
-});
-
-
 Route::view('/about','about');
 Route::view('/contact', 'contact');
+
+Route::resource('jobs',JobController::class);
+
