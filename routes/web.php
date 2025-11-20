c49e05133a7caf95df534faea52fa84843a2da62
LAMPEngineer <lamp.engineer@gmail.com>
2025-11-20 11:02:37 +0530

    register login blade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
@@ -10,4 +12,12 @@ Route::view('/about','about');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs',JobController::class);
+
+// Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+// Login
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
